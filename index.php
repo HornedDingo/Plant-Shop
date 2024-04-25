2e12358167872ee0cd181978d9aa3bdd82c834ef
HornedDingo <samsonova.s.yu@gmail.com>
2024-04-25 14:15:55 +0300

    Structure

---
+++ index.php
@@ -0,0 +1,22 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Plant Shop</title>
+    <link rel="stylesheet" href="style.css"/>
+</head>
+<body>
+    <!-- header -->
+    <header class="" id="">
+        <!-- -->
+    </header>
+    <!-- main -->
+    <main class="" id="">
+        <!-- -->
+    </main>
+    <!-- footer -->
+    <footer class="" id="">
+        <!-- -->
+    </footer>
+</body>
+</html>
